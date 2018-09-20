953555c7b9300909bc0c6509a7b48150f4e7cabc
LZF666 <850215855@qq.com>
2018-09-20 16:26:01 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1809/86/deb33800de224c02.jpg_750x200_74fb795e.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1712/51/36632b2cb850e902.jpg_750x200_fa4120db.jpg'
+        }
+      ]
+    }
+  },
+  swiperList: [
+    {
+      id: '0001',
+      imgUrl:
+        'http://img1.qunarzz.com/piao/fusion/1809/86/deb33800de224c02.jpg_750x200_74fb795e.jpg'
+    },
+    {
+      id: '0002',
+      imgUrl:
+        'http://img1.qunarzz.com/piao/fusion/1712/51/36632b2cb850e902.jpg_750x200_fa4120db.jpg'
+    }
+  ]
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff !important
+.wrapper
+  width 100%
+  height 0
+  overflow hidden
+  padding-bottom 26.6%
+  background-color #ccc
+  .swiper-img
+    width 100%
+</style>

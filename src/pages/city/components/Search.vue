aa3a1ad9f988f5f741dc1a9263636dec36db6c38
LZF666 <850215855@qq.com>
2018-09-23 23:55:47 +0800

    city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,27 +1,71 @@
 <template>
-<div class="search">
- <input type="text" name="" id="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" name="" id="search-input" placeholder="输入城市名或拼音" v-model="keyword">
+    </div>
+    <div class="search-content">
+      <ul>
+        <li v-for="item of list" :key="item.id">{{item.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  prpos: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword > -1) || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 .search
-  height .72rem
-  padding 0 .1rem
+  height 0.72rem
+  padding 0 0.1rem
   background $bgColor
   #search-input
     box-sizing border-box
     width 100%
-    height .62rem
-    line-height .62rem
+    height 0.62rem
+    line-height 0.62rem
     text-align center
-    border-radius .06rem
+    border-radius 0.06rem
     color #666
-    padding 0 .1rem
+    padding 0 0.1rem
+.search-content
+  z-index 1
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background green
 </style>
